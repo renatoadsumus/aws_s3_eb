e1d00fd91f8c15bf6f07160f1a96550a0deb7e91
renatoadsumus <renatoadsumus@gmail.com>
2018-05-29 10:59:37 -0300

    Realizando clone do App para alterar valor do Token

--- jenkins/gerar_token.sh
+++ jenkins/gerar_token.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
+git config --global github.token 8c8d821c7878aa978c6d502c8d55926f4b54d9d5
+git config --global user.name "renatoadsumus"
+git config --global github.user "renatoadsumus"
+git config --global user.email "renatoadsumus@gmail.com"
+
+git clone https://github.com/renatoadsumus/geru_app.git
+
+cd geru_app
 rm -fr aws_eb/Dockerrun.aws.json
+
 cp -u aws_eb/Dockerrun.aws.template.json aws_eb/Dockerrun.aws.json
 token=`date +%s | sha256sum | base64 | head -c 32`
 echo "Novo Token Gerado: " $token
 sed -i -e "s/TROCAR/$token/g" aws_eb/Dockerrun.aws.json
-#git config --global github.token 8c8d821c7878aa978c6d502c8d55926f4b54d9d5
-#git config --global user.name "renatoadsumus"
-#git config --global github.user "renatoadsumus"
-#git config --global user.email "renatoadsumus@gmail.com"
+git status
 #git commit -a -m "Alterando novo valor de token"
 #git push
