eb5236da9944044b20678a01dd29e3656a34a072
renatoadsumus <renatoadsumus@gmail.com>
2018-05-29 09:52:23 -0300

    Removendo espacao da variavel no script

--- jenkins/gerar_token.sh
+++ jenkins/gerar_token.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp -u aws_eb/Dockerrun.aws.template.json aws_eb/Dockerrun.aws.json
-token = `date +%s | sha256sum | base64 | head -c 32`
+token=`date +%s | sha256sum | base64 | head -c 32`
 echo $token
 sed -i -e "s/TROCAR/$token/g" aws_eb/Dockerrun.aws.json
-git commit -m -a "Alterando novo valor de token"
+git commit -a -m "Alterando novo valor de token"
