5a3b1fd215913643a6f916a4f10e7125ac602dbb
renatoadsumus <renatoadsumus@gmail.com>
2018-05-29 10:09:56 -0300

    Incluindo o comando git push para novo token no arquivo Dockerrun.aws.json

--- jenkins/gerar_token.sh
+++ jenkins/gerar_token.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp -u aws_eb/Dockerrun.aws.template.json aws_eb/Dockerrun.aws.json
 token=`date +%s | sha256sum | base64 | head -c 32`
-echo $token
+echo "Novo Token Gerado: " $token
 sed -i -e "s/TROCAR/$token/g" aws_eb/Dockerrun.aws.json
 git commit -a -m "Alterando novo valor de token"
+git push
